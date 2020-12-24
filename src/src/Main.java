04962f18b9866b136a678b5c204177fcfce385f5
Rahel81 <rahimova81@gmail.com>
2020-12-24 11:00:16 +0300

    added Main.class

---
+++ src/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15_000;
+        int rate = 20;
+
+        int miles = price / rate;
+        System.out.println(miles);
+    }
+}
